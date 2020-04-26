d1ed44bcca62793b4dd3395d6a17ed618301c656
mihir9455 <raimihir10@gmail.com>
2020-04-26 19:10:34 +0530

    First commit

---
+++ OnlineTest/src/main/java/com/capgemini/onlinetest/model/User.java
@@ -0,0 +1,39 @@
+package com.capgemini.onlinetest.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+	@Id
+	private Long userId;
+	private String userName;
+	private Boolean isAdmin;
+	private String userPassword;
+	public Long getUserId() {
+		return userId;
+	}
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public Boolean getIsAdmin() {
+		return isAdmin;
+	}
+	public void setIsAdmin(Boolean isAdmin) {
+		this.isAdmin = isAdmin;
+	}
+	public String getUserPassword() {
+		return userPassword;
+	}
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+	
+
+}
